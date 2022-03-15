cfda34c181cf29b7ed8ec805206baeb9d7903257
marceleon <marcelo.a.leon@gmail.com>
2022-03-15 20:18:24 -0300

    FRONT VERSION 3

---
+++ src/redux/actions/authActions.js
@@ -0,0 +1,79 @@
+import axios from 'axios';
+import { API } from '../../helpers/api'
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
+
+toast.configure()
+export const authActions = {
+    newUser: (user) => {
+        return async (dispatch, getState) => {
+            try {
+                console.log(user)
+                const response = await axios.post(API + '/user', user)
+
+                console.log(response.data);
+                if (response.data.validatorErrors) {
+                    return response.data.validatorErrors //joi validator
+                } else if (!response.data.success) {
+                    return response.data.errorsValidator.details
+                } else {
+                    dispatch({
+                        type: 'LOG_USER',
+                        payload: response.data.response
+                    })
+                    toast.success("You've been registered!", {position: toast.POSITION.TOP_RIGHT})
+                }
+            } catch (error) {
+                toast.error('Internal database error, try in a moment please', {position: toast.POSITION.TOP_RIGHT})
+            }
+        }
+    },
+
+    logIn: (user) => {
+        return async (dispatch, getState) => {
+            try {
+                const response = await axios.post(API + '/login', user)
+
+                if (response.data.success) {
+                    dispatch({
+                        type: 'LOG_USER',
+                        payload: response.data,
+                    })
+                } else {
+                    toast.error('Email and/or password incorrect', {position: toast.POSITION.TOP_RIGHT})
+                }
+            } catch (error) {
+                toast.error('Internal database error, try in a moment please', {position: toast.POSITION.TOP_RIGHT})
+            }
+        }
+    },
+
+    logOut: () => {
+        return (dispatch, getState) => {
+            dispatch({
+                type: 'LOG_OUT'
+            })
+        }
+    },
+
+    logInWithLocalStorage: (user) => {
+        return async (dispatch, getState) => {
+            try {
+                const response = await axios.get(API + '/signinls', {
+                    headers: {
+                        'Authorization': `Bearer ${user.token}`
+                    }
+                })
+                dispatch({
+                    type: 'LOG_USER',
+                    payload: {
+                        ...response.data.response,
+                        token: user.token
+                    }
+                })
+            } catch (error) {
+                toast.error('Internal database error, try in a moment please', {position: toast.POSITION.TOP_RIGHT})
+            }
+        }
+    }
+}
